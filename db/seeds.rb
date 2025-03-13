d5ceb58a03901af433a00880766046011e588f44
Peter BH <hpb1317@gmail.com>
2025-03-13 13:46:43 +0700

    feat: remove seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@
 #   end
 #
 #
-User.create!(
-  email: "admin@gmail.com",
-  password: "password",
-  password_confirmation: "password"
-)
+# User.create!(
+#   email: "admin@gmail.com",
+#   password: "password",
+#   password_confirmation: "password"
+# )
